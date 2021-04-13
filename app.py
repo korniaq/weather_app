353e42a3e698e527bd22699eba4916b44194b7ec
korniaq <79938120+korniaq@users.noreply.github.com>
2021-04-13 19:50:43 +0200

    Add files via upload
    adding a new city is now possible

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
 from flask import Flask
+from flask import request
 import sys
 from flask import render_template
 import requests
+import json
 
 app = Flask(__name__)
+API_KEY = '7a1c45cb67aa9811d480c28f2c51dddf'
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
+    if request.method == 'POST':
+        city = request.form['city_name'].upper()
+        r = requests.get('https://api.openweathermap.org/data/2.5/weather?q={}&appid={}'.format(city, API_KEY))
+        dict = json.loads(r.text)
+        dict_with_weather_info = {'degrees': int(dict['main']['temp'])-273, 'state': dict['weather'][0]['main'], 'city': city}
+        return render_template('index.html', weather=dict_with_weather_info)
+
     return render_template("index.html")
-
-
-@app.route('/add', methods=['POST'])
-def add_city():
-    pass
 
 
 # don't change the following way to run flask:
