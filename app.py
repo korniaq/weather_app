3723f8a3fa77f46b3d859b5056cf781bca1f5753
korniaq <79938120+korniaq@users.noreply.github.com>
2021-04-15 19:26:03 +0200

    Add files via upload
    added sqlalchemy database

--- app.py
+++ app.py
@@ -1,24 +1,83 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request, render_template
 import sys
-from flask import render_template
-import requests
 import json
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String
+from datetime import datetime, timezone
+import requests
 
 app = Flask(__name__)
+SQLALCHEMY_DATABASE_URL = 'sqlite:////tmp/weather.db'
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True, connect_args={'check_same_thread': False})
+SessionLocal = sessionmaker(bind=engine)
+session = SessionLocal()
+Base = declarative_base()
 API_KEY = '7a1c45cb67aa9811d480c28f2c51dddf'
 
 
-@app.route('/', methods=['POST', 'GET'])
-def index():
-    if request.method == 'POST':
-        city = request.form['city_name'].upper()
-        r = requests.get('https://api.openweathermap.org/data/2.5/weather?q={}&appid={}'.format(city, API_KEY))
-        dict = json.loads(r.text)
-        dict_with_weather_info = {'degrees': int(dict['main']['temp'])-273, 'state': dict['weather'][0]['main'], 'city': city}
-        return render_template('index.html', weather=dict_with_weather_info)
+class City(Base):
+    __tablename__ = 'City'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False, unique=True)
+    def __repr__(self):
+        return '<User %r>' % self.username
 
-    return render_template("index.html")
+
+Base.metadata.drop_all(engine) #<--- to clear the database
+Base.metadata.create_all(engine)
+
+
+def get_weather(city):
+    r = requests.get('https://api.openweathermap.org/data/2.5/weather?q={}&appid={}'.format(city, API_KEY))
+    time_now = int(datetime.now(tz=timezone.utc).timestamp())
+    day_time = get_daytime(time_now, r.json())
+    dict_with_weather_info = {'degrees': int(json.loads(r.text)['main']['temp']) - 273,
+                              'state': json.loads(r.text)['weather'][0]['main'],
+                              'city': city,
+                              'day_time': day_time}
+    return dict_with_weather_info
+
+
+def get_daytime(time, response):
+    hr_gap = 1
+    if response['sys']['sunrise'] < time <= response['sys']['sunset'] - 3600 * hr_gap:
+        return "day"
+    # if hr_gap before and after sunrise or hr_gap before after sunset
+    elif response['sys']['sunrise'] - 3600 * hr_gap < time < response['sys']['sunrise'] + 3600 * hr_gap or \
+            response['sys']['sunset'] - 3600 * hr_gap < time < response['sys']['sunset'] + 3600 * hr_gap:
+        return "evening-morning"
+    else:
+        return "night"
+
+
+@app.route('/')
+def index():
+    cities_weather = []
+    for city in session.query(City).order_by(City.id):
+        cities_weather.append(get_weather(city.name))
+    return render_template("index.html", cities=cities_weather)
+
+
+@app.route('/', methods=['POST'])
+def add_city():
+    new = request.form['city_name'].upper()
+    city_already_there = False
+    for city in session.query(City).order_by(City.id):
+        if new == city.name:
+            city_already_there = True
+
+    if not city_already_there:
+        new_city = City(name=new)
+        session.add(new_city)
+        session.commit()
+
+    cities_weather = []
+    for city in session.query(City).order_by(City.id):
+        cities_weather.append(get_weather(city.name))
+
+    return render_template('index.html', cities=cities_weather)
 
 
 # don't change the following way to run flask:
